288c122065b3344960a65a8466c2d56f9522a40b
Chris Bowdon <c.bowdon@gmail.com>
2012-04-22 15:46:24 +0100

    made a mutable version of init-permutations and started the parallel implementation with racket/future

--- pfannkuchen-single-file.rkt
+++ pfannkuchen-single-file.rkt
@@ -1,8 +1,10 @@
 #lang racket/base
 
 (require racket/list
+         racket/vector
          racket/contract
-         racket/cmdline)
+         racket/cmdline
+         racket/future)
 
 (provide (contract-out 
           [pfannkuchen (-> exact-positive-integer? pair?)]))
@@ -19,19 +21,19 @@
 ;; just holds a permutation: state, sign and count array
 (struct perm (p s c))
 
-;; initializes n-1 permutations - this will be used for parallelization
+;; initializes n-1 permutations - used for parallelization
 ;; isn't actually used in the serial implementation
-;(define (init-permutations n)
-;  (let ([n-1 (sub1 n)])
-;    (define (ip-iter i p c all)      
-;      (if (= i n)
-;          all
-;          (let ([p2 (rotate-left-first-n p n)]
-;                [c2 (sub1-at-index c n-1)])
-;            (ip-iter (add1 i) p2 c2 (append all (list (perm p2 1 c2)))))))    
-;    (let ([p0 (build-list n (λ (x) (+ 1 x)))] 
-;          [c0 (build-list n (λ (x) (+ 1 x)))])             
-;      (ip-iter 1 p0 c0 (list (perm p0 1 c0))))))
+(define (init-permutations! n)
+  (let ([n-1 (sub1 n)])
+    (define (ip-iter i p c all)      
+      (if (= i n)
+          (reverse all)
+          (let ([p2 (rotate-left-first-n! (vector-copy p) n)]
+                [c2 (sub1-at-index! (vector-copy c) n-1)])
+            (ip-iter (add1 i) p2 c2 (cons (perm p2 1 c2) all)))))    
+    (let ([p0 (build-vector n (λ (x) (+ 1 x)))] 
+          [c0 (build-vector n (λ (x) (+ 1 x)))])             
+      (ip-iter 1 p0 c0 (cons (perm p0 1 c0) '())))))
 
 ;; generate the next permutation (has side effects!)
 ;; (-> exact-nonnegative-integer? perm? perm?)
@@ -48,8 +50,8 @@
         (np-iter! 2 (swap-at-indices! (perm-p op) 1 2) (perm-c op)))      
       (perm (swap-at-indices! (perm-p op) 0 1) -1 (perm-c op))))
 
-;; return list that is identical to lst but with 1 subtracted from the nth element
-;; (-> (listof number?) number? (listof number?))
+;; subtract 1 from the nth element
+;; (-> vector? number? vector?)
 (define (apply-to-index! proc v index)
   (begin 
     (vector-set! v index (proc (vector-ref v index)))
@@ -58,11 +60,11 @@
 (define (sub1-at-index! c i)
   (apply-to-index! sub1 c i))
 
-;; straightforward, get a copy of list with val at index set to new val
+;; set val at index to new val
 (define (set-at-index! v i j)
   (begin (vector-set! v i j) v))
 
-;; (-> (listof number?) number? (listof number?)) 
+;; (-> vector? number? vector?) 
 (define (rotate-left-first-n! v n)
   (let ([temp (vector-ref v 0)])
     (vector-copy! v 0 v 1 n)
@@ -70,14 +72,20 @@
     v))
 
 ;; returns array but with swapped values at indices
-;; (-> list? exact-nonnegative-integer? exact-nonnegative-integer? list?)
+;; (-> vector? exact-nonnegative-integer? exact-nonnegative-integer? vector?)
 (define (swap-at-indices! v i j)
   (let ([temp (vector-ref v i)])
     (vector-set! v i (vector-ref v j))
     (vector-set! v j temp)
     v))
 
-
+;; factorial
+;; (-> exact-nonnegative-integer?  exact-nonnegative-integer?)
+(define (factorial x)
+  (define (f-iter y result)
+    (cond [(= 0 y) result]
+          [else (f-iter (sub1 y) (* y result))]))
+  (f-iter x 1))
 
 ;;;;;;;;;;;;;;;;;; PANCAKE FLIPPING
 
@@ -95,8 +103,6 @@
           number-of-flips
           (multi-flip (single-flip lst (drop lst l) l) (add1 number-of-flips)))))  
   (multi-flip input 0))
-
-
 
 ;;;;;;;;;;;;;;;;;; MAIN
 
@@ -116,24 +122,56 @@
                          (if [> flips maxflips] flips maxflips)))))  
         (pf-iter (perm orig-p 1 orig-c) 0 0))))
 
-(time (pfannkuchen (command-line #:program "pfannkuchen"
-                                 #:args (n)
-                                 (string->number n))))
+;; parallel version 
+
+;; parallel version auxiliaries
+(define (limited-pfannkuchen op limit)
+  (define (pf-iter p checksum maxflips count)          
+    (if [or (not p) (>= count limit)]
+        (cons checksum maxflips)              
+        (let ([flips (fannkuch (vector->list (perm-p p)))])
+          (pf-iter (next-permutation! p)
+                   ((if [> (perm-s p) 0] + -) checksum flips)
+                   (if [> flips maxflips] flips maxflips)
+                   (add1 count)))))  
+  (pf-iter op 0 0 0))
+
+;; testing - it works
+(define (parts n)
+  (map
+   (λ (x) (limited-pfannkuchen x (factorial (sub1 n))))
+   (init-permutations! n)))
+
+(define z (command-line #:program "pfannkuchen"
+                        #:args (n)
+                        (string->number n)))
+
+(define ans (pfannkuchen z))
+(require rackunit)
+(check-equal? (foldl (λ (x y) (+ (car x) y)) 0 (parts z)) (car ans))
+(check-equal? (apply max (map cdr (parts z))) (cdr ans))
+
+(define (tasks n)
+  (map (λ (x) (future (λ () (limited-pfannkuchen x (factorial (sub1 n))))))
+       (init-permutations! n)))
+
+;; NOT parallel actually...
+(define para (map touch (tasks z)))
+
+(check-equal? (foldl (λ (x y) (+ (car x) y)) 0 para) (car ans))
+(check-equal? (apply max (map cdr para)) (cdr ans))
+
+(require lib/time)
+(time-repeat (pfannkuchen z) #:repeat 1)
+;(time-repeat (map touch (tasks z)) #:repeat 1)
 
 ;; test-times Intel Corei5, OS X Lion (MBP 2011)
 ;; shows that mutable permutation version is slightly faster
 ;; n : mut, immut
 ;; 12: 901s, 1038s
-;; 11: 65s, 81s
+;; 11: 65s, 81s, 
 ;; 10 : 4.8s, 6.3s
 ;; 9 : 0.4s, 0.6s
-
-
-;; Test timings on an Intel Core i3, Win 7 (Dell Studio 1558)
-;; 12 : 1304s
-;; 11 : 100s
-;; 10 : 8.1s
-;; 9 : 0.7s
 
 ;; TODO
 ;; Parallelize this
@@ -149,6 +187,7 @@
 ;; --> (listof (cons checksum maxflips))
 ;; ---> answers are (foldl + 0 n-checksums) (max n-maxflips)
 ;; where pfannkuchen-of-next-(n-1)!-permutations is just pfannkuchen above,
-;; but using (next-k-permutations op k) where k = (n-1)!
+;; but limited to (n-1)! permutations
 ;; Presumably some kind of concurrent mutable data structure will be required
 ;; for the final step of adding the checksums and maxflips
+
